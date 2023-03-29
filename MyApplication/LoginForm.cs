5c9ff243e44fd50673267fe8d799e7b83bca6481
p Nosrati <poorya1417@yahoo.com>
2023-03-29 21:00:08 +0430

    Login

--- MyApplication/LoginForm.cs
+++ MyApplication/LoginForm.cs
@@ -12,7 +12,31 @@ public partial class LoginForm : Infrastructure.BaseForm
 	#endregion /Constructors
 
 	#region Events
-	#region Reset
+	#region FormLoad
+	private void LoginForm_Load(object sender, EventArgs e)
+	{
+		Persistence.DatabaseContext? databaseContext = null;
+
+		try
+		{
+			databaseContext =
+				new Persistence.DatabaseContext();
+		}
+		catch (Exception ex)
+		{
+			var errorMessage =
+				$"Error Message: {ex.Message}";
+
+			Infrastructure.MyMessageBox.ErrorMessageBox(text: errorMessage);
+		}
+		finally
+		{
+			databaseContext?.Dispose();
+		}
+	}
+	#endregion /FormLoad
+
+	#region ResetMethod
 	/// <summary>
 	/// Reset
 	/// </summary>
@@ -21,12 +45,104 @@ public partial class LoginForm : Infrastructure.BaseForm
 		usernameTextBox.Text = string.Empty;
 		passwordTextBox.Text = string.Empty;
 	}
-	#endregion /Reset
+	#endregion /ResetMethod
 
 	#region LoginButtun
 	private void LoginButton_Click(object sender, EventArgs e)
 	{
+		usernameTextBox.Text =
+			Infrastructure.Utility
+			.FixText(text: usernameTextBox.Text);
 
+		passwordTextBox.Text =
+			Infrastructure.Utility
+			.FixText(text: passwordTextBox.Text);
+
+		if (usernameTextBox.Text == string.Empty || passwordTextBox.Text == string.Empty)
+		{
+			var errorMessage =
+				"Username and Password are required!";
+
+			Infrastructure.MyMessageBox.ErrorMessageBox(text: errorMessage);
+
+			if (usernameLabel.Text == string.Empty)
+			{
+				usernameTextBox.Focus();
+			}
+			else
+			{
+				passwordTextBox.Focus();
+			}
+
+			return;
+		}
+
+		Persistence.DatabaseContext? databaseContext = null;
+
+		try
+		{
+			databaseContext =
+				new Persistence.DatabaseContext();
+
+			var foundedUser =
+				databaseContext.Users
+				.Where(current => current.Username.ToLower() == usernameTextBox.Text.ToLower())
+				.FirstOrDefault();
+
+			if (foundedUser == null)
+			{
+				var errorMessage =
+					"Username and/or Password is not correct!";
+
+				Infrastructure.MyMessageBox.ErrorMessageBox (text: errorMessage);
+
+				usernameTextBox.Focus();
+
+				return;
+			}
+
+			if (string.Compare(foundedUser.Password,
+				passwordTextBox.Text, ignoreCase: false) != 0)
+			{
+				var errorMessage =
+					"Username and/or Password is not correct!";
+
+				Infrastructure.MyMessageBox.ErrorMessageBox(text: errorMessage);
+
+				usernameTextBox.Focus();
+
+				return;
+			}
+
+			if (foundedUser.IsActive == false)
+			{
+				var errorMessage =
+					"You can not login right now! Please contact support.";
+
+				Infrastructure.MyMessageBox.ErrorMessageBox(text:errorMessage);
+
+				usernameTextBox.Focus();
+
+				return;
+			}
+		}
+
+		catch (Exception ex)
+		{
+			var errorMessage =
+				$"ErrorMessage:{ex.Message}";
+
+			Infrastructure.MyMessageBox.ErrorMessageBox(text: errorMessage);
+		}
+
+		finally
+		{
+			databaseContext?.Dispose();
+		}
+
+		MainForm mainForm = new MainForm();
+		mainForm.Show();
+		this.Hide();
 	}
 	#endregion /LoginButtun
 
@@ -60,28 +176,5 @@ public partial class LoginForm : Infrastructure.BaseForm
 	}
 	#endregion /ExitButton
 
-	#region FormLoad
-	private void LoginForm_Load(object sender, EventArgs e)
-	{
-		Persistence.DatabaseContext? databaseContext = null;
-
-		try
-		{
-			databaseContext =
-				new Persistence.DatabaseContext();
-		}
-		catch (Exception ex)
-		{
-			var errorMessage =
-				$"Error Message: {ex.Message}";
-
-			Infrastructure.MyMessageBox.ErrorMessageBox(text: errorMessage);
-		}
-		finally
-		{
-			databaseContext?.Dispose();
-		}
-	}
-	#endregion /FormLoad
 	#endregion /Events
 }
